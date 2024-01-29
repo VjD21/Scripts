e27b20f5b422b7f17fa5038cfd01f0342f3a839e
vijaydurai <vijayduraiaws@gmail.com>
2024-01-29 22:39:16 +0530

    hostname entry correction

--- set_hostname.sh
+++ set_hostname.sh
@@ -6,9 +6,9 @@ if [ "$current_hostname" == "ip-192-168-2-10" ]; then
     sudo hostnamectl set-hostname master.vijaydurai21.site
 
 elif [ "$current_hostname" == "ip-192-168-2-11" ]; then
-    sudo hostnamectl set-hostname worker1.vijayduar21.site
+    sudo hostnamectl set-hostname worker1.vijaydurai21.site
     
 elif [ "$current_hostname" == "ip-192-168-2-12" ]; then
-    sudo hostnamectl set-hostname worker2.vijayduar21.site
+    sudo hostnamectl set-hostname worker2.vijaydurai21.site
 
 fi
